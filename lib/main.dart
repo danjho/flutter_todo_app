1afab6976a184827a2c3121c58d8fca96c5f3f44
Dan Jhonatan <dan@mentorspace.dev>
2022-03-12 14:52:12 -0300

    Adicionado base do tema do app

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:todo_app/routes/app_pages.dart';
 import 'package:todo_app/routes/app_routes.dart';
+import 'package:todo_app/themes/app_theme.dart';
 
 void main() {
   final app = GetMaterialApp(
     title: 'Flutter ToDo App',
     debugShowCheckedModeBanner: false,
+    theme: AppTheme.light,
     initialRoute: AppRoutes.login,
     getPages: AppPages.pages,
     locale: const Locale('pt', 'BR'),
---
+++ lib/themes/app_theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  AppTheme._();
+
+  static final light = ThemeData.light().copyWith(
+    scaffoldBackgroundColor: Color(0xFFF2F7FF),
+    cardColor: Color(0xFFFEFFFF),
+  );
+}
