3f643d353c9cdd189ef935c091f397b60db311e4
Dan Jhonatan <dan@mentorspace.dev>
2022-03-13 15:38:51 -0300

    Adicionado `.env`

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
 import 'package:todo_app/routes/app_pages.dart';
 import 'package:todo_app/routes/app_routes.dart';
 import 'package:todo_app/themes/app_theme.dart';
 
-void main() {
+Future<void> main() async {
+  await dotenv.load(fileName: '.env');
+
   final app = GetMaterialApp(
     title: 'Flutter ToDo App',
     debugShowCheckedModeBanner: false,
--- lib/modules/login/presenter/login_binding.dart
+++ lib/modules/login/presenter/login_binding.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
 import 'package:todo_app/modules/login/data/repositories/login_repository.dart';
 import 'package:todo_app/modules/login/externals/data_sources/api_login_data_source.dart';
@@ -7,8 +8,9 @@ import 'package:todo_app/modules/login/presenter/login_controller.dart';
 class LoginBinding implements Bindings {
   @override
   void dependencies() {
+    final apiAddress = dotenv.env['API_ADDRESS'];
     final datasource = ApiLoginDataSource(
-      dio: Dio(BaseOptions(baseUrl: '')),
+      dio: Dio(BaseOptions(baseUrl: apiAddress!)),
     );
     final repo = LoginRepository(datasource: datasource);
     Get.lazyPut(() => LoginController(repo));
