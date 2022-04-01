27de807a2e6283bab2735b07c0b622d8e64a4cea
Dan Jhonatan <dan@mentorspace.dev>
2022-03-31 21:07:20 -0300

    Card de uma tarefa

--- lib/modules/tasks/presenter/home/local_widgets/task_card.dart
+++ lib/modules/tasks/presenter/home/local_widgets/task_card.dart
@@ -1,24 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:todo_app/core/utils/constants.dart';
-import 'package:todo_app/modules/tasks/data/models/task_model.dart';
+import 'package:todo_app/modules/tasks/domain/entities/task.dart';
 import 'package:todo_app/modules/tasks/presenter/home/local_widgets/task_check_box.dart';
 
 class TaskCard extends StatelessWidget {
   const TaskCard({
     required this.task,
     this.onTap,
+    this.color,
     Key? key,
   }) : super(key: key);
 
-  final TaskModel task;
-  final Function(TaskModel task)? onTap;
+  final Task task;
+  final Color? color;
+  final Function(Task task)? onTap;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(DEFAULT_PADDING),
-      margin: EdgeInsets.symmetric(vertical: DEFAULT_PADDING / 2),
+      margin: EdgeInsets.symmetric(vertical: DEFAULT_PADDING / 4),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(16),
         color: Colors.white,
