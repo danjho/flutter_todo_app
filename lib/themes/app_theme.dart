9d2cd6a61d47e87a5a5bc297ceb5121935c3c814
Dan Jhonatan <dan@mentorspace.dev>
2022-04-30 15:56:14 -0300

    Atualizando cores do tema

--- lib/themes/app_theme.dart
+++ lib/themes/app_theme.dart
@@ -4,6 +4,8 @@ class AppTheme {
   AppTheme._();
 
   static final light = ThemeData.light().copyWith(
+    primaryColor: Color(0xFF0A235E),
+    backgroundColor: Colors.grey[300],
     scaffoldBackgroundColor: Color(0xFFF2F7FF),
     cardColor: Color(0xFFFEFFFF),
   );
